2a6723e20579becd862d81121594851a1ab9dcf4
Ressiuuu <beres.bakti.ui.ac.id>
2023-09-07 21:18:50 +0700

    halo

---
+++ Repo_TP/penjumlahan.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class penjumlahan {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukkan angka pertama: ");
+        float a = scanner.nextFloat();
+
+        System.out.print("Masukkan angka kedua: ");
+        float b = scanner.nextFloat();
+
+        float perkalian = a * b;
+        float pembagian = a / b;
+        float penjumlahan = a + b;
+        float pengurangan = a - b;
+
+        System.out.println("Hasil perkalian: " + perkalian);
+        System.out.println("Hasil pembagian : " + pembagian);
+        System.out.println("Hasil penjumlahan: " + penjumlahan);
+        System.out.println("Hasil Pengurangan : " + pengurangan);
+    }
+}
